fefcaa0de2568c93f690c4dac299b91518eb3547
Tariq <chill.tariq@gmail.com>
2024-02-26 23:03:17 -0500

    add more tests

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ import (
 var SLICES_PER_PIZZA = 8
 var EXTRA_CHEESE_SLICES = 2
 
-func TestSlicesWithExtraSlicesToWholePizzas(t *testing.T) {
+func TestSlicesToWholePizzasWithExtraSlices(t *testing.T) {
 	slices := 36 + EXTRA_CHEESE_SLICES
 	pizzas, remainingSlices := slicesToWholePizzas(slices, SLICES_PER_PIZZA)
 	assert.Equal(t, 4, pizzas)
@@ -34,6 +34,33 @@ func TestSlicesWithExtraSlicesToWholePizzas(t *testing.T) {
 	pizzas, remainingSlices = slicesToWholePizzas(slices, SLICES_PER_PIZZA)
 	assert.Equal(t, 1, pizzas)
 	assert.Equal(t, 2, remainingSlices)
+
+	slices = 6 + EXTRA_CHEESE_SLICES
+	pizzas, remainingSlices = slicesToWholePizzas(slices, SLICES_PER_PIZZA)
+	assert.Equal(t, 1, pizzas)
+	assert.Equal(t, 0, remainingSlices)
+}
+
+func TestSlicesToWholePizzas(t *testing.T) {
+	slices := 36
+	pizzas, remainingSlices := slicesToWholePizzas(slices, SLICES_PER_PIZZA)
+	assert.Equal(t, 4, pizzas)
+	assert.Equal(t, 4, remainingSlices)
+
+	slices = 8
+	pizzas, remainingSlices = slicesToWholePizzas(slices, SLICES_PER_PIZZA)
+	assert.Equal(t, 1, pizzas)
+	assert.Equal(t, 0, remainingSlices)
+
+	slices = 1
+	pizzas, remainingSlices = slicesToWholePizzas(slices, SLICES_PER_PIZZA)
+	assert.Equal(t, 0, pizzas)
+	assert.Equal(t, 1, remainingSlices)
+
+	slices = 0
+	pizzas, remainingSlices = slicesToWholePizzas(slices, SLICES_PER_PIZZA)
+	assert.Equal(t, 0, pizzas)
+	assert.Equal(t, 0, remainingSlices)
 }
 
 func TestPizzasToOrder(t *testing.T) {
